076225cab64651db8da60a4b86b93b95897a4d69
Arthur Curty <arthur.vieira@lavid.ufpb.br>
2018-07-24 13:41:56 -0300

    adicionando atividade 1 com testes + makefile

--- Atividade 1/Insertion_Sort/Insertion_sort.cpp
+++ Atividade 1/Insertion_Sort/Insertion_sort.cpp
@@ -1,44 +1,53 @@
 #include <iostream>
 #include <stdlib.h>
+#include <fstream>
 
-using namespace std;
 
 void insertin_sort(int num[],int tam){
-   for(int i=1;i<tam;i++){
-    int atual = num[i];
-    int j=i-1;
-        while((j>=0)&&(num[j]>atual))
-        {
-            num[j+1] = num [j];
-            j--;
+
+   for(int i=1;i<tam;i++)
+       {
+            int atual = num[i];
+            int j=i-1;
+
+                while((j>=0)&&(num[j]>atual))
+                {
+                    num[j+1] = num [j];
+                    j--;
+                }
+                num[j+1]=atual;
+
         }
-        num[j+1]=atual;
-    }
+
+    for (int i = 0; i < tam; ++i)
+        {
+            std::cout << "Posicao: " << i+1 << " -> Numero: " << num[i] << std::endl;
+        }
+
 }
 
 
 int main(){
-    int tamanho;
-    cin>>tamanho;
-    int vetor[tamanho];
+    std::string entrada;
+	int array[100000];
+	int linhas = 0;
 
-    for(int i=0;i<tamanho;i++){
-        cin>>vetor[i];
-    }
+	std::cout << "Digite o nome do arquivo contendo os casos de teste: " << std::endl;
+	std::cin >> entrada;
 
-     for(int i=0;i<tamanho;i++){
-        cout<<vetor[i]<<" ";
-        }
+	 std::ifstream file;
+	 file.open(entrada);
+	 char output[1000];
+	 if (file.is_open()) {
+	 while (!file.eof()) {
+	    file >> output;
+	    array[linhas] = atoi(output);
+	   	linhas++;
+	 }
+	}
+	file.close();
 
-        insertin_sort(vetor,tamanho);
-
-     for(int i=0;i<tamanho;i++){
-        cout<<vetor[i]<<" ";
-        }
-
-
-
-
+    insertin_sort(array, linhas);
 
 
 return 0;
--- Atividade 1/Selection_Sort/Selection_sort.cpp
+++ Atividade 1/Selection_Sort/Selection_sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fstream>
 
 using namespace std;
 
@@ -21,32 +22,36 @@ void selection_sort(int num[],int tam){
         num[i] = num[Min];
         num[Min] = aux;
     }
+        for (int i = 0; i < tam; ++i)
+        {
+            std::cout << "Posicao: " << i+1 << " -> Numero: " << num[i] << std::endl;
+        }
+
 }
 
 int main(){
 
-        int tamanho;
-        cin>>tamanho;
-        int vetor[tamanho];
+  std::string entrada;
+	int array[100000];
+	int linhas = 0;
 
-        for(int i=0;i<tamanho;i++)
-            {
-                cin>>vetor[i];
-            }
+	std::cout << "Digite o nome do arquivo contendo os casos de teste: " << std::endl;
+	std::cin >> entrada;
 
-        cout<<"Vetor ordenado: ";
-            for(int i=0;i<tamanho;i++)
-                {
-                 cout<<vetor[i]<<" ";
-                }
+	 std::ifstream file;
+	 file.open(entrada);
+	 char output[1000];
+	 if (file.is_open()) {
+	 while (!file.eof()) {
+	    file >> output;
+	    array[linhas] = atoi(output);
+	   	linhas++;
+	 }
+	}
+	file.close();
 
-        selection_sort(vetor,tamanho);
+    selection_sort(array, linhas);
 
-         cout<<"Vetor ordenado: ";
-            for(int i=0;i<tamanho;i++)
-                {
-                 cout<<vetor[i]<<" ";
-                }
 
 return 0;
 }
